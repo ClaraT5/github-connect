b22bedb7ee465ef5c4db69adde9385c97b0e3377
Clara B <claratamsin5@gmail.com>
2023-08-14 10:21:19 -0400

    Initial Commit

---
+++ github connect/github_connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_connectApp.swift
+//  github connect
+//
+//  Created by Clara T Brown on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct github_connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
